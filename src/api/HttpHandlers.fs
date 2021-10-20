e2a80258b40356e7945dac73743071d9f0deca95
Thorben Werner Sjøstrøm Dahl <thorben.dahl@nrk.no>
2021-10-20 10:18:47 +0200

    Validate the date the user requested

--- src/api/HttpHandlers.fs
+++ src/api/HttpHandlers.fs
@@ -4,6 +4,21 @@ module HttpHandlers =
 
     open Microsoft.AspNetCore.Http
     open Giraffe
+    open System
+    open System.Globalization
+    open System.Threading.Tasks
+
+    let parseAsDateTime (dateAsString: string) : DateTime option =
+        try
+            let date =
+                DateTime.ParseExact(dateAsString, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None)
+
+            Some date
+        with
+        | _ -> None
 
     let epgHandler (dateAsString: string) : HttpHandler =
-        fun (next: HttpFunc) (ctx: HttpContext) -> (json dateAsString) next ctx
+        fun (next: HttpFunc) (ctx: HttpContext) ->
+            match (parseAsDateTime dateAsString) with
+            | Some date -> (json date) next ctx
+            | None -> RequestErrors.badRequest (text "Invalid date") (Some >> Task.FromResult) ctx
